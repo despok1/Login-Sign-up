8c84e79128eec9c88c84f4a8e715508ec914e3de
qudmen <qudmenyt@gmail.com>
2024-11-10 14:12:00 +0200

    Форму

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,42 +3,68 @@
 </script>
 
 <template>
-  <div class="section a-lot-height modal-dialog-centered">
-    <div class="container text-center">
+  <div class="container">
+    <!-- Switcher -->
+    <section class="center-position">
       <div class="row">
+        <div>
+          <span>LOG IN</span> <span class="micro-padding">SIGN UP</span>
+        </div>
         <div class="form-check form-switch">
           <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-          <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
         </div>
       </div>
-      <div class="row">
-        <div class="row mb-3">
-          <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control form-control-sm" id="colFormLabelSm"
-              placeholder="col-form-label-sm">
-          </div>
+    </section>
+    <section class="center-position">
+      <div class="block-form text-center">
+        <h4>Log In</h4>
+        <div>
+          <input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail"
+            autocomplete="off">
         </div>
-        <div class="row mb-3">
-          <label for="colFormLabel" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="colFormLabel" placeholder="col-form-label">
-          </div>
+        <div>
+          <input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass"
+            autocomplete="off">
         </div>
-        <div class="row">
-          <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control form-control-lg" id="colFormLabelLg"
-              placeholder="col-form-label-lg">
-          </div>
+        <!-- Submit -->
+        <div>
+          <button class="button-submit-shiny button-submit">Submit</button>
         </div>
+        <div class="forgot-password-shiny">Forgot your password?</div>
       </div>
-    </div>
+      <!-- INPUTS -->
+    </section>
   </div>
 </template>
 
 <style scoped>
-.a-lot-height {
-  height: 1000px;
+.center-position {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.micro-padding {
+  padding-left: 30px;
+}
+
+.block-form {
+  width: 400px;
+  /* height: 350px; */
+  background: #343541;
+}
+
+.forgot-password-shiny:hover {
+  color: yellow;
+}
+
+.button-submit-shiny:hover {
+  background-color: rgb(33, 60, 133);
+}
+
+.button-submit {
+  border-radius: 10px 10px 10px 10px;
+  -webkit-border-radius: 10px 10px 10px 10px;
+  -moz-border-radius: 10px 10px 10px 10px;
 }
 </style>
